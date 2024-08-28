428e3563e4f1c3a57798a5f18784f53e8b8bb14a
Steve <mas.ste@gmail.com>
2024-08-28 13:46:17 +0200

    Fixed sphinx requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,7 @@ author = 'Steve Mas'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_copybutton',
-    'sphinx_rtd_theme',
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,5 +23,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_static_path = ['_static']
